8a87fba2440c7a82f36b59fb6879aa14e5ba5d6d
Yousef Mahmoud <mryoyo133@gmail.com>
2023-10-12 16:47:48 +0300

    renaming variables

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
 
 # window
-win = pygame.display.set_mode((600, 700))
+WIN = pygame.display.set_mode((600, 700))
 pygame.display.set_caption("Pixels")
 
 # var
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-gray = (200, 200, 200)
-blue = (0, 0, 255)
-light_blue = (0, 255, 255)
-green = (0, 255, 0)
-yellow = (255, 255, 0)
-brown = (117, 76, 41)
-bg = pygame.image.load("bg.png")
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GRAY = (200, 200, 200)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (0, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BROWN = (117, 76, 41)
+BG = pygame.image.load("BG.png")
+PIXEL_WIDTH = 20
+
 pixels= []
-pixel_width = 20
-color = black
+color = BLACK
 
 #class
 class pixel(object):
@@ -32,11 +33,11 @@ class pixel(object):
 
 #screen 
 def redrawscreen():
-    pygame.draw.rect(win, white, (0, 0, 600, 600))
+    pygame.draw.rect(WIN, WHITE, (0, 0, 600, 600))
     for s in pixels:
-        s.draw(win)
-    win.blit(bg, (0, 600))
-    pygame.draw.circle(win, gray, (mx, my), pixel_width, 1)
+        s.draw(WIN)
+    WIN.blit(BG, (0, 600))
+    pygame.draw.circle(WIN, GRAY, (mx, my), PIXEL_WIDTH, 1)
 
 #main loop
 run = True
@@ -47,37 +48,40 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 4:
-                pixel_width += 1
-            if pixel_width>1:
+                PIXEL_WIDTH += 1
+            if PIXEL_WIDTH>1:
                 if event.button == 5:
-                    pixel_width -= 1        
+                    PIXEL_WIDTH -= 1        
             
     click = pygame.mouse.get_pressed()
     if click[0] == 1:
-        pixels.append(pixel(mx, my, pixel_width, color))    
+        pixels.append(pixel(mx, my, PIXEL_WIDTH, color))    
     if click[2] == 1:
-        pixels.append(pixel(mx, my, pixel_width, white))  
+        pixels.append(pixel(mx, my, PIXEL_WIDTH, WHITE))  
     keys = pygame.key.get_pressed()
     if keys[pygame.K_1]:
-        color =black
+        color =BLACK
     elif keys[pygame.K_2]:
-        color =gray
+        color =GRAY
     elif keys[pygame.K_3]:
-        color =red
+        color =RED
     elif keys[pygame.K_4]:
-        color =blue  
+        color =BLUE  
     elif keys[pygame.K_5]:
-        color =green
+        color =GREEN
     elif keys[pygame.K_6]:
-        color =yellow
+        color =YELLOW
     elif keys[pygame.K_7]:
-        color = brown
+        color = BROWN
     elif keys[pygame.K_8]:
-        color = light_blue 
+        color = LIGHT_BLUE 
     elif keys[pygame.K_ESCAPE]:
         run = False
     elif keys[pygame.K_LCTRL]:
         if keys[pygame.K_z]:
-            del pixels[-1:]
+            try:
+                pixels.pop()
+            except:
+                pass
     redrawscreen()
     pygame.display.update()
